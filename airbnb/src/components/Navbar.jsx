87e11ddfb13948f88b4bca02f89f17af6884bd42
Faisal baig <fasalbaig255@gmail.com>
2024-09-14 17:13:21 +0530

    new changes

--- airbnb/src/components/Navbar.jsx
+++ airbnb/src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import { Button } from "./ui/button"
+import Link from "react-router-dom"
 
 const Navbar = () => {
   return (
@@ -11,6 +12,7 @@ const Navbar = () => {
             <a href="#" className="text-gray-500 hover:text-gray-700">Help</a>
             <a href="#" className="text-gray-500 hover:text-gray-700">Sign up</a>
             <a href="#" className="text-gray-500 hover:text-gray-700">Log in</a>
+            <Link to='/shortlist'>Shortstay</Link>
           </nav>
           <Button variant="ghost" className="md:hidden">
             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><line x1="3" y1="12" x2="21" y2="12"/><line x1="3" y1="6" x2="21" y2="6"/><line x1="3" y1="18" x2="21" y2="18"/></svg>
